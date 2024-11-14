21ee58f56f1497353398859185f6ae78d22a72f9
SardorbekSaydamatov <saidalo.saydamatov@gmail.com>
2024-11-14 15:11:10 +0500

    Initial Commit

---
+++ SpeedReader/SpeedReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedReaderApp.swift
+//  SpeedReader
+//
+//  Created by Sardorbek Saydamatov on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
